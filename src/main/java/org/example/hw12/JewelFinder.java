ac69ad8de484cb369d07195855ec44ba3d636310
Serhii Vietrov <sb.vetrov@gmail.com>
2023-10-22 20:27:18 +0300

    var name fixed

--- src/main/java/org/example/hw12/JewelFinder.java
+++ src/main/java/org/example/hw12/JewelFinder.java
@@ -3,11 +3,11 @@ package org.example.hw12;
 public class JewelFinder {
 
     public int numJewelsInStones(String jewels, String stones) {
-        char[] jewelsSinglyUniq = jewels.toCharArray();
+        char[] jewelsSingly = jewels.toCharArray();
         char[] stonesSingly = stones.toCharArray();
         int counter = 0;
 
-        for (char j : jewelsSinglyUniq) {
+        for (char j : jewelsSingly) {
             for (char s : stonesSingly) {
                 if (j == s) {
                     counter++;
